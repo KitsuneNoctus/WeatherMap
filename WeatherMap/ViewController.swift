424a9ea358b32cac8db4088252bf6d7f0f4ea0c8
Henry Calderon <theceintiant@gmail.com>
2020-06-04 21:52:05 -0700

    Initial Commit

---
+++ WeatherMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMap
+//
+//  Created by Henry Calderon on 6/4/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
